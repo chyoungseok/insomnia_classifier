5e3c5e84657077e8026185d2f467f2aa07e5ca41
chyoungseok <chyoungseok@hanyang.ac.kr>
2024-12-01 14:50:55 +0900

    gen epoch npy database & modify my_dataset.py

--- my_dataset.py
+++ my_dataset.py
@@ -1,40 +1,24 @@
 import os
 import numpy as np
-from sklearn.preprocessing import OneHotEncoder
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader, random_split
 
 # Custom Dataset 생성
 class gen_dataset(Dataset):
-    def __init__(self, num_classes=2, sampling_rate=34.13, train_or_test='default'):
-
-        if 'train' in train_or_test.lower():
-            path_load = 'Y:/MESA_0.7.0/ppg_preprocessed_npy/train'
-        elif 'test' in train_or_test.lower():
-            path_load = 'Y:/MESA_0.7.0/ppg_preprocessed_npy/test'
-        else:
-            path_load = 'Y:/MESA_0.7.0/ppg_preprocessed_npy'
+    def __init__(self, num_classes=2, sampling_rate=34.13, path_load = 'Y:/MESA_0.7.0/ppg_preprocessed_epoch_npy'):
                     
-        self.data = np.load(os.path.join(path_load, "ppg_ins_hc.npy"))
-
-        labels = np.load(os.path.join(path_load, "label_ins_hc.npy")).reshape(-1, 1)
-        # One-hot encoder 생성 및 적용
-        encoder = OneHotEncoder(sparse_output=False)
-        self.labels = encoder.fit_transform(labels)
-
-        self.mask = np.load(os.path.join(path_load, "mask_ins_hc.npy"))
+        self.data = torch.from_numpy(np.load(os.path.join(path_load, "ppg_epochs.npy")))
+        self.label = torch.from_numpy(np.load(os.path.join(path_load, "label_epochs.npy")))
 
         self.num_samples = self.data.shape[0]
         self.num_classes = num_classes
         self.sampling_rate = sampling_rate 
-        self.train_or_test = train_or_test
 
     def __len__(self):
         return self.num_samples
 
     def __getitem__(self, idx):
-        signal = self.data[idx]
-        label = self.labels[idx]
-        # mask = self.mask[idx]
-        # PyTorch 텐서로 변환
-        return torch.tensor(signal, dtype=torch.float32).unsqueeze(0), torch.tensor(label, dtype=torch.long) #, torch.tensor(mask, dtype=torch.float32).unsqueeze(0)
+        data = self.data[idx]
+        label = self.label[idx]
+
+        return data, label
--- my_model.py
+++ my_model.py
@@ -114,3 +114,116 @@ class SimpleSleepPPGModel_2(nn.Module):
         mask = mask[:, :, :output_length * self.pool.stride:self.pool.stride]  # stride에 맞춰 자르기
         return x, mask
 
+
+# -*- coding = utf-8 -*
+# @Time：  10:07
+# @File: MAF_CNN.py
+# @Software: PyCharm
+import torch
+import torch.nn as nn
+
+# MAF-CNN === Start =========================== 
+class SELayer(nn.Module):
+    def __init__(self, channel=32, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        print("SE input (x) by concat: ([32, 3], [32, 2], [32, 3]): ", x.shape)
+        b, c, _ = x.size()
+        print("b, c: {}, {}".format(b, c))
+        y = self.avg_pool(x).view(b, c)
+        print("after squeeze (squeeze the last dimension):", y.shape)
+        y = self.fc(y).view(b, c, 1)
+        print("after exication (y): ", y.shape)
+        print("y.expand_as(x): ", y.expand_as(x).shape)
+        return x * y.expand_as(x)
+
+class MSA(nn.Module):
+    def __init__(self):
+        super(MSA, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(64, 32, (5,), (1,), dilation=(2,)),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv1d(64, 32, (4,), (1,), dilation=(3,)),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv1d(64, 32, (3,), (1,), dilation=(4,)),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+        )
+        self.se = SELayer()
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        # print(x1.shape)
+        x2 = self.conv2(x)
+        # print(x2.shape)
+        x3 = self.conv3(x)
+        # print(x3.shape)
+        out = torch.cat([x1, x2, x3], dim=2)
+        # print(out.shape)
+        out = self.se(out)
+        # print(out.shape)
+        return out
+
+class MAF_CNN(nn.Module):
+    def __init__(self, num_classes):
+        super(MAF_CNN, self).__init__()
+        self.cnn1 = nn.Sequential(
+            nn.Conv1d(1, 64, (256,), (32,)),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+
+            nn.MaxPool1d(8, 8),
+
+            nn.Dropout(),
+
+            nn.Conv1d(64, 128, (7,), (1,)),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Conv1d(128, 64, (7,), (1,)),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+
+            nn.MaxPool1d(4, 4),
+        )
+
+        self.dropout = nn.Dropout()
+        self.msa = MSA()
+        self.ft = nn.Flatten()
+        self.fc = nn.Sequential(
+            nn.Linear(256, 512),
+            nn.ReLU(),
+            nn.Linear(512, 128),
+            nn.ReLU(),
+        )
+        self.out = nn.Linear(128, num_classes)
+
+    def forward(self, x1):
+        x1 = self.cnn1(x1)
+        print("after CNN: ", x1.shape)
+
+        x1 = self.msa(x1)
+        print("after MSA (x * y.expand_as(x)): ", x1.shape)
+
+        x_concat = x1
+        # print(x_concat.shape)
+        x = self.dropout(x_concat)
+        x = self.ft(x)
+
+        out = self.fc(x)
+        x = self.out(out)
+        return out, x
+# MAF-CNN === End =========================== 
